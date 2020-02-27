f15d2b4b7b7dfcbc126386f181532a8165004c60
Sean Birmingham <seannbirmingham98@gmail.com>
2020-02-27 17:28:35 -0500

    set up userContext and userProvider, added useLocalStorage hook, edited a few components

---
+++ umts/src/components/AddListing.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddListing = () => {
+  return (
+    <div>
+      <h2>New Listing</h2>
+    </div>
+  );
+};
+
+export default AddListing;
---
+++ umts/src/components/Dashboard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Dashboard = () => {
+  return (
+    <div>
+      <h2>Discover Products</h2>
+    </div>
+  );
+};
+
+export default Dashboard;
